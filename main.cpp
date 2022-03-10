e70e249f78dbf80d51fe2b2984fc83bdbed786bb
unknown <you@example.com>
2022-03-10 23:28:33 +0400

    Переписала рекурсию на итерацию

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int fib(int n) {
-	if (n == 1)
-		return 0;
-	if (n == 2)
-		return 1;
-	return fib(n - 1) + fib(n - 2);
+void fib(int n) {
+	int a = 0, b = 1;
+	for (int i = 0; i < n; ++i) {
+		cout << a << endl;
+		int t = b;
+		b += a;
+		a = t;
+	}
 }
 
 int main() {
-	cout << fib(10);
+	fib(10);
 	return 0;
 }
